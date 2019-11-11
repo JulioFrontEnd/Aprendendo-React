54232245053e1c3da3f6cd675b523f29db8f4d79
Your Name <you@example.com>
2019-11-11 20:36:18 -0300

    Aprendendo a adicionar components

---
+++ src/components/LittleComponent/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.css';
+const LittleComponent = () => (
+    <div id='lc'>Little Component</div>
+);
+
+export default LittleComponent;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import LittleComponent from './components/LittleComponent';
+import './index.css';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LittleComponent />, document.getElementById('l'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
