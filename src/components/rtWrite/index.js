9598cbcee62128154c59580a5658d43f72f14458
Your Name <you@example.com>
2019-11-19 19:34:10 -0300

    add component

--- src/components/rtWrite/index.js
+++ src/components/rtWrite/index.js
@@ -1,63 +1,83 @@
 import React,{Component} from 'react';
 import "./index.css";
 
-class IfOffline extends Component{
-    render(){
-        return(
-            <div className='l logoutCamp'>
-                <h3>VOCÊ NÃO ESTÁ LOGADO!, FAÇA LOGIN PARA CONTINUAR!</h3>
-            </div>
-        );
-    }
-}
-class IfOnline extends Component{
-    render(){
-        return(
-            <div className='l loginCamp'>
-                <h3>VOCÊ ESTÁ LOGADO!, Seja bem vindo {this.props.name}</h3>
+// class IfOffline extends Component{
+//     render(){
+//         return(
+//             <div className='l logoutCamp'>
+//                 <h3>VOCÊ NÃO ESTÁ LOGADO!, FAÇA LOGIN PARA CONTINUAR!</h3>
+//             </div>
+//         );
+//     }
+// }
+// class IfOnline extends Component{
+//     render(){
+//         return(
+//             <div className='l loginCamp'>
+//                 <h3>VOCÊ ESTÁ LOGADO!, Seja bem vindo {this.props.name}</h3>
                 
-            </div>
-        );
-    }
-}
+//             </div>
+//         );
+//     }
+// }
 
-class Verify extends Component{
+// class Verify extends Component{
     
-    render(){
-        let btn=''; 
-        if(this.props.name !== '' && !this.props.c){
-            btn = <IfOnline name={this.props.name}/>
-        }else{
-            btn = <IfOffline/>
-        }
-        return(
-        <div>{btn}</div>
-        );
-    }
+//     render(){
+//         let btn=''; 
+//         if(this.props.name !== '' && !this.props.c){
+//             btn = <IfOnline name={this.props.name}/>
+//         }else{
+//             btn = <IfOffline/>
+//         }
+//         return(
+//         <div>{btn}</div>
+//         );
+//     }
     
-}
+// }
 
 export default class RtWrite extends Component {
     state = {
       name: '',
       nameLoad: false,
+      currentMessage: '',
     }; 
+
+    componentDidMount(){
+        this.ifOffline()
+    }
     
     handleInputChange = () => {
       this.setState({ name: document.getElementById("inpute").value }); 
     };
 
-    handleSend = () => {
-        this.setState({nameLoad:true});
+    handleSend = async () => {
+        await this.setState({nameLoad:true});
+        this.verify();
+    };
+    logout = async () => {
+        await this.setState({nameLoad:false});
+        this.verify();
     };
 
-    
+    ifOnline = ()=>{
+        this.setState({currentMessage: <div className='l loginCamp'><h3>VOCÊ ESTÁ LOGADO!, Seja bem vindo {this.state.name}</h3><button onClick={this.logout}>Deslogar</button></div>});
+    }
+    ifOffline = ()=>{
+        this.setState({currentMessage: <div className='l logoutCamp'><h3>VOCÊ NÃO ESTÁ LOGADO!, FAÇA LOGIN PARA CONTINUAR!</h3></div>});
+        
+    }
+    verify = ()=>{
+        if(this.state.nameLoad){
+            return this.ifOnline()
+        }else{
+            return this.ifOffline()
+        }
+    }
     
     
     render() {
-        const logout = () =>{
-            this.setState({nameLoad:false});
-        }
       return (
         <div>
             <div className='l entrada'>
@@ -69,8 +89,8 @@ export default class RtWrite extends Component {
             <br/>
             <br/>
             
-            <Verify name={this.state.name} c={this.state.nameLoad}/>
-            <button onClick={logout}>Deslogar</button>
+            {this.state.currentMessage}
+            
                 
             
         </div>
